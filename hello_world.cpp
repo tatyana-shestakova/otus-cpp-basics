0f76e860395df900c8306de54406ae35aeab579c
tatyana-shestakova <tatyana_sch@list.ru>
2023-10-09 20:57:50 +0300

    feat: add new files

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include "move_me_out.h"
+#include "example.h"
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+
+	example_function();
+	todo_move_me_out();
+	
+	return 0;
+}
